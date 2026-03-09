237375df6c2435c16323e1a09ae1f13a51b0f658
chuprovatata <chuprova_tata@mail.ru>
2026-03-10 02:39:40 +0300

    Merge

--- app/src/main/java/com/example/datingapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/datingapp/navigation/AppNavigation.kt
@@ -1,6 +1,6 @@
 package com.example.datingapp.navigation
 
-import NotificationScreen
+import com.example.datingapp.screens.notification.NotificationScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
